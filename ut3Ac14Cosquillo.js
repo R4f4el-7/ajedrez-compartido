d195f5b7d9b63ec25a8260295a1a882972f406f1
aplic <cosquillo00@gmail.com>
2025-11-04 19:57:06 +0100

    let y const

--- ut3Ac14Cosquillo.js
+++ ut3Ac14Cosquillo.js
@@ -1,5 +1,5 @@
-piezas = {"peon" : 1, "caballo" : 3, "alfil" : 3, "torre" : 5, "dama" : 9};
+const piezas = {"peon" : 1, "caballo" : 3, "alfil" : 3, "torre" : 5, "dama" : 9};
 
-total =  piezas.peon + piezas.caballo + piezas.alfil;
+let total =  piezas.peon + piezas.caballo + piezas.alfil;
 
 console.log(total.toFixed(1));
--- ut3Act19Cosquillo.js
+++ ut3Act19Cosquillo.js
@@ -1,4 +1,4 @@
-posicion ={"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
+let posicion ={"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8}
 function esMovimientoValido(pieza, origen, destino){
     switch (pieza) {
         case "♙": // Peón
