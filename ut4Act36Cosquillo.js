8c467964ef1608c676fa651905ec53b16e42e1c1
Rafael Cosquillo <cosquillo00@gmail.com>
2025-11-13 09:28:55 +0100

    Introduccion del 36

--- ut4Act36Cosquillo.js
+++ ut4Act36Cosquillo.js
@@ -26,5 +26,17 @@ const tablero = [
 //Muestre el tablero en consola.
 mostrarTablero(tablero);
 //Permita al usuario (por prompt()) elegir una pieza y moverla a otra posición
-moverPieza(tablero, [6, 4], [4, 4]);
+let posicion ={"a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8};
+
+let origen = prompt("Posicionde de origen(ej. e4, e5 ...): ");
+let destino = prompt("Posicionde de destino(ej. e4, e5 ...): ");
+
+columna_origen = posicion[origen.slice(0,1)];
+fila_origen = parseInt(origen.slice(1,2));
+
+columna_destino = posicion[origen.slice(0,1)];
+fila_destino = parseInt(origen.slice(1,2));
+
+
+moverPieza(tablero, [fila_origen, columna_origen], [fila_destino, columna_destino]);
 mostrarTablero(tablero);
