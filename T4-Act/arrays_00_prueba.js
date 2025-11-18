983ad3494ce87ed3c7eb87a0af020ae0b92d31fd
aplic <cosquillo00@gmail.com>
2025-11-18 20:56:37 +0100

    prueba metodos arr

--- T4-Act/arrays_00_prueba.js
+++ T4-Act/arrays_00_prueba.js
@@ -17,9 +17,10 @@ console.log(`Se elimina el primer movimiento e7: ${movimientos}`);
 
 //clases y objeto literal equivalencia
 class Persona {
-    constructor(nombre, apellido) {
+    constructor(nombre, apellido, edad) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.edad = edad;
     }
     saludar() {
         console.log(`Buenos dias ${this.nombre} ${this.apellido}`);
@@ -40,18 +41,38 @@ perdona2.saludar();
 //array de objetos (persona)
 
 const arr_persona_clase = [
-    new Persona("nombre1", "Apellido1"),
-    new Persona("nombre2", "Apellido2"),
-    new Persona("nombre3", "Apellido3"),
-    new Persona("nombre4", "Apellido4"),
+    new Persona("nombre1", "Apellido1", 1),
+    new Persona("nombre2", "Apellido2", 4),
+    new Persona("nombre3", "Apellido3", 3),
+    new Persona("nombre4", "Apellido4", 4),
 ];
 console.log(arr_persona_clase);
 
 const arr_persona_objLiteral = [
-    {nombre:"nombre1", apellido:"apellido1"},
-    {nombre:"nombre2", apellido:"apellido3"},
-    {nombre:"nombre3", apellido:"apellido3"},
-    {nombre:"nombre4", apellido:"apellido4"},
+    {nombre:"nombre1", apellido:"apellido1", edad:1},
+    {nombre:"nombre2", apellido:"apellido3", edad:2},
+    {nombre:"nombre3", apellido:"apellido3", edad:3},
+    {nombre:"nombre4", apellido:"apellido4", edad:4},
 ];
 
 console.log(arr_persona_objLiteral);
+
+//array de objetos filter/sort/map/reduce
+
+const arr_persona_conteoEdad = arr_persona_clase
+    .reduce((acc, p) => {
+        if (acc[p.edad]){
+            acc[p.edad] = acc[p.edad] + 1;
+        }else{
+            acc[p.edad] = 1
+        }
+        return acc;
+    },{})
+
+console.log(arr_persona_conteoEdad);
+
+const arr_persona_edadDoble = arr_persona_clase.map(a => a.edad * 2);
+console.log(arr_persona_edadDoble);
+
+const arr_persona_sin1 = arr_persona_clase.filter(p => p.nombre !== "nombre1");
+console.log(arr_persona_sin1);
